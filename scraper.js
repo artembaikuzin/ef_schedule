dfe1928ca62732bfb11ae46769d021022d84e81f
Artem Baikuzin <ybinzu@gmail.com>
2018-07-20 18:35:49 +0300

    Get schedule directly from DOM

--- scraper.js
+++ scraper.js
@@ -11,8 +11,6 @@ const settings = require('./settings');
 
   // Go straight away to the online booking page
   await page.goto('https://ec.ef.com/evc/gl?icid=School.GroupClass.2012&lng=en');
-
-  const evalSettings = settings;
   
   await page.evaluate((settings) => {
     document.querySelector("#username").value = settings.EF_LOGIN;
@@ -20,37 +18,17 @@ const settings = require('./settings');
   }, settings);
 
   const navPromise = page.waitForNavigation();
-  
   await page.click('.etc-login-btn');
   await navPromise;
 
-  page.on('response', async response => {
-    if (response.request().method() === 'POST' &&
-      response.url().startsWith('https://ec.ef.com/services/api/proxy/queryproxy') &&
-      response.request().postData().includes('Anncouncement_')) {
-        
-      const data = await response.json();
+  // Wait for appearance of the checkbox "I would like a video class"
+  await page.waitForSelector('.evc-layout-videooption-text', { visible: true });
 
-      console.log('TEXT', await response.text());
-      console.log('DATA: ', data)
-      
-      storage.saveCurrentWeek(parseSchedule(data));
-    }
+  const schedule = await page.evaluate(() => {
+    const rawSchedule = document.querySelectorAll('.evc-all-class-list-wrap td em');
+    return Array.from(rawSchedule).map(item => { return item.innerText; });
   });
 
-  // Wait for appearance of the checkbox "I would like a video class"
-  await page.waitForSelector('.evc-layout-videooption-text');
+  storage.saveCurrentWeek(schedule);
   await browser.close();
 })();
-
-function parseSchedule(rawData) {
-  // FIXME: possible errors because of the absolute indexing
-  return rawData.filter(item => {
-      return item.translation !== undefined;
-    })
-    .slice(0, 7)
-    .reverse()
-    .map(item => {
-      return item.translation;
-    });
-}
